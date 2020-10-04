c1bd96c2094afae7408fdab09c3162533032b60c
michaeltran8 <59845826+michaeltran8@users.noreply.github.com>
2020-10-04 18:24:26 -0400

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -7,7 +7,7 @@ package ca.sheridancollege.project;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
+ * @author michael, Joshua, Chunantai 2020
  */
 public abstract class Player 
 {
